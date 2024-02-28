1a31004d69d8da75b0908dafd24c3685bfa9f9da
sihanm2 <sihanm2@uw.edu>
2024-02-28 11:15:42 -0800

    done

--- Assignment-3/routes/studentInformation.js
+++ Assignment-3/routes/studentInformation.js
@@ -9,8 +9,15 @@ const studentInformation = express.Router();
  */
 studentInformation.get("/students", (req, res) => {
   try {
+    const userIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    const userDevice = req.header("User-Agent");
+
     if (data) {
-      res.json(data);
+      res.json({
+        response: data,
+        userIP,
+        userDevice,
+      });
     } else {
       res.status(404).json({
         message: "No student information is available now.",
@@ -28,11 +35,18 @@ studentInformation.get("/students", (req, res) => {
  */
 studentInformation.post("/search-students-by-id", (req, res) => {
   try {
+    const userIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    const userDevice = req.header("User-Agent");
+
     const { studentId } = req.body;
     const foundStudent = data.find((s) => s.student_id === studentId);
 
     if (foundStudent) {
-      res.json(foundStudent);
+      res.json({
+        response: foundStudent,
+        userIP,
+        userDevice,
+      });
     } else {
       res.status(404).json({
         message:
@@ -51,13 +65,20 @@ studentInformation.post("/search-students-by-id", (req, res) => {
  */
 studentInformation.post("/students-CS548", (req, res) => {
   try {
+    const userIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    const userDevice = req.header("User-Agent");
+
     const cs548Students = data.filter((s) =>
       s.courses.some((c) => c.course_id === "CS548")
     );
 
     if (cs548Students) {
       const studentIds = cs548Students.map((student) => student.student_id);
-      res.json(studentIds);
+      res.json({
+        response: studentIds,
+        userIP,
+        userDevice,
+      });
     } else {
       res.status(400).json({
         message: "No student is currently taking CS548.",
@@ -75,19 +96,50 @@ studentInformation.post("/students-CS548", (req, res) => {
  */
 studentInformation.post("/students-same-courses", (req, res) => {
   try {
+    const userIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    const userDevice = req.header("User-Agent");
+
     const { studentId } = req.body;
-    const myCourses = data.filter((s) => s.student_id === studentId)[0].courses;
-    const myCourseIds = myCourses
-      .map((c) => c.course_id)
-      .filter((c) => c !== "CS548");
+    const student = data.filter((s) => s.student_id === studentId);
 
-    const sameCourseStudents = data.filter(
-      (s) =>
-        s.student_id !== studentId &&
-        s.courses.some((c) => myCourseIds.includes(c.course_id))
-    );
+    if (student.length !== 0) {
+      const myCourses = student[0].courses;
 
-    res.json(sameCourseStudents);
+      if (myCourses.length !== 0) {
+        const myCourseIds = myCourses
+          .map((c) => c.course_id)
+          .filter((c) => c !== "CS548");
+
+        const sameCourseStudents = data.filter(
+          (s) =>
+            s.student_id !== studentId &&
+            s.courses.some((c) => myCourseIds.includes(c.course_id))
+        );
+
+        if (sameCourseStudents) {
+          res.json({
+            response: sameCourseStudents,
+            userIP,
+            userDevice,
+          });
+        } else {
+          res.status(400).json({
+            message:
+              "No one is taking the same course (excluding CS548) with the given students.",
+          });
+        }
+      } else {
+        res.status(400).json({
+          message:
+            "Could not find the courses of the students with the given student id.",
+        });
+      }
+    } else {
+      res.status(404).json({
+        message:
+          "Student not found. Please enter a different student id or check your spelling.",
+      });
+    }
   } catch (err) {
     res.status(500).json({
       message: "Server error occured. Please try again later.",
